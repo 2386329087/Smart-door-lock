6711eb7e0934d6ab96d501890da80f90f5f57e3a
1696774082 <1696774082@qq.com>
2023-07-05 11:01:21 +0800

    修改main

--- User/main.c
+++ User/main.c
@@ -21,73 +21,26 @@
 #include "lvgl.h"
 #include "lv_port_disp.h"
 /* Global define */
-#define TASK1_TASK_PRIO     5
-#define TASK1_STK_SIZE      256
-#define TASK2_TASK_PRIO     5
-#define TASK2_STK_SIZE      256
+
 
 /* Global Variable */
-TaskHandle_t Task1Task_Handler;
-TaskHandle_t Task2Task_Handler;
+TaskHandle_t lvgl_tick_Task_Handler;
+TaskHandle_t lvgl_timer_Task_Handler;
 
-
-/*********************************************************************
- * @fn      GPIO_Toggle_INIT
- *
- * @brief   Initializes GPIOA.0/1
- *
- * @return  none
- */
-void GPIO_Toggle_INIT(void)
-{
-  GPIO_InitTypeDef  GPIO_InitStructure={0};
-
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
-}
-
-
-/*********************************************************************
- * @fn      task1_task
- *
- * @brief   task1 program.
- *
- * @param  *pvParameters - Parameters point of task1
- *
- * @return  none
- */
-void task1_task(void *pvParameters)
+void lvgl_tick_task(void *pvParameters)
 {
     while(1)
     {
         printf("task1 entry\r\n");
-        GPIO_SetBits(GPIOA, GPIO_Pin_0);
-        vTaskDelay(250);
-        GPIO_ResetBits(GPIOA, GPIO_Pin_0);
         vTaskDelay(250);
     }
 }
 
-/*********************************************************************
- * @fn      task2_task
- *
- * @brief   task2 program.
- *
- * @param  *pvParameters - Parameters point of task2
- *
- * @return  none
- */
-void task2_task(void *pvParameters)
+void lvgl_timer_task(void *pvParameters)
 {
     while(1)
     {
-        printf("task2 entry\r\n");
-        GPIO_ResetBits(GPIOA, GPIO_Pin_1);
-        vTaskDelay(500);
-        GPIO_SetBits(GPIOA, GPIO_Pin_1);
+        //printf("task2 entry\r\n");
         vTaskDelay(500);
     }
 }
@@ -113,21 +66,8 @@ int main(void)
     lv_init();
     lv_port_disp_init();
     //ui_init();
-	GPIO_Toggle_INIT();
-	/* create two task */
-    xTaskCreate((TaskFunction_t )task2_task,
-                        (const char*    )"task2",
-                        (uint16_t       )TASK2_STK_SIZE,
-                        (void*          )NULL,
-                        (UBaseType_t    )TASK2_TASK_PRIO,
-                        (TaskHandle_t*  )&Task2Task_Handler);
-
-    xTaskCreate((TaskFunction_t )task1_task,
-                    (const char*    )"task1",
-                    (uint16_t       )TASK1_STK_SIZE,
-                    (void*          )NULL,
-                    (UBaseType_t    )TASK1_TASK_PRIO,
-                    (TaskHandle_t*  )&Task1Task_Handler);
+    xTaskCreate(lvgl_tick_task,"lvgl_tick_task",2000,NULL,5,&lvgl_tick_Task_Handler);
+    xTaskCreate(lvgl_timer_task,"lvgl_timer_task",3600,NULL,15,&lvgl_timer_Task_Handler);
     vTaskStartScheduler();
 
 	while(1)
