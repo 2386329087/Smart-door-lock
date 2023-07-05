2a94b5ce5b4003069741cb94ea7e91a304bfad2c
1696774082 <1696774082@qq.com>
2023-07-05 13:40:52 +0800

    测试系统，提高频率

--- User/FreeRTOSConfig.h
+++ User/FreeRTOSConfig.h
@@ -97,7 +97,7 @@
 #define configTICK_RATE_HZ				( ( TickType_t ) 500 )
 #define configMAX_PRIORITIES			( 15 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 256 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 12 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 30 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 16 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
--- User/main.c
+++ User/main.c
@@ -20,19 +20,26 @@
 #include "task.h"
 #include "lvgl.h"
 #include "lv_port_disp.h"
+#include "semphr.h"
 /* Global define */
 
 
 /* Global Variable */
 TaskHandle_t lvgl_tick_Task_Handler;
 TaskHandle_t lvgl_timer_Task_Handler;
-
+SemaphoreHandle_t uart2_mutex_handler;
 void lvgl_tick_task(void *pvParameters)
 {
     while(1)
     {
-        printf("task1 entry\r\n");
-        vTaskDelay(250);
+        if(xSemaphoreTake(uart2_mutex_handler,pdMS_TO_TICKS(10))==pdPASS){
+            printf("task1 entry\r\n");
+            xSemaphoreGive(uart2_mutex_handler);
+        }
+
+        // printf("task1 entry\r\n");
+        lv_tick_inc(2);
+        vTaskDelay(pdMS_TO_TICKS(2));
     }
 }
 
@@ -40,8 +47,13 @@ void lvgl_timer_task(void *pvParameters)
 {
     while(1)
     {
-        //printf("task2 entry\r\n");
-        vTaskDelay(500);
+        if(xSemaphoreTake(uart2_mutex_handler,pdMS_TO_TICKS(10))==pdPASS){
+            printf("task2 entry\r\n");
+            xSemaphoreGive(uart2_mutex_handler);
+        }
+
+        // printf("task2 entry\r\n");
+        vTaskDelay(pdMS_TO_TICKS(16));
     }
 }
 
@@ -66,8 +78,9 @@ int main(void)
     lv_init();
     lv_port_disp_init();
     //ui_init();
-    xTaskCreate(lvgl_tick_task,"lvgl_tick_task",2000,NULL,5,&lvgl_tick_Task_Handler);
-    xTaskCreate(lvgl_timer_task,"lvgl_timer_task",3600,NULL,15,&lvgl_timer_Task_Handler);
+    uart2_mutex_handler= xSemaphoreCreateMutex();
+    xTaskCreate(lvgl_tick_task,"lvgl_tick_task",2000,NULL,14,&lvgl_tick_Task_Handler);
+    xTaskCreate(lvgl_timer_task,"lvgl_timer_task",3200,NULL,5,&lvgl_timer_Task_Handler);
     vTaskStartScheduler();
 
 	while(1)
