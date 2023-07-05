d71d21962b167ee7546bfb2f08a828376b4f010f
1696774082 <1696774082@qq.com>
2023-07-05 17:50:56 +0800

    修正文件编码

--- Hardware/IIC.h
+++ Hardware/IIC.h
@@ -7,7 +7,7 @@ void IIC_Init( u32 bound , u16 address );
 u8 IIC_WaitEvent(I2C_TypeDef* I2Cx, uint32_t I2C_EVENT);
 
 u8 IIC_ReadByte(u8 addr,u8 reg);
-//ÏÂÁĞº¯Êı´ı²âÊÔ
+
 u8 IIC_WriteLen(u8 addr, u8 reg, u8 len, u8 *buf);
 u8 IIC_ReadLen(u8 addr, u8 reg, u8 len, u8 *buf);
 #endif
