e692ceed91594f572bcaa6b8d9e9e20cecbb6204
1696774082 <1696774082@qq.com>
2023-07-08 14:48:26 +0800

    创建温度采集任务

--- User/main.c
+++ User/main.c
@@ -26,13 +26,16 @@
 #include "DHT11.h"
 /* Global define */
 
-
+#define mutex(mutex_handler,wait_ms,code) if(xSemaphoreTake(mutex_handler,pdMS_TO_TICKS(wait_ms))==pdPASS){\
+            code   \
+            xSemaphoreGive(mutex_handler);\
+        }  
 /* Global Variable */
 lv_group_t *group;
 TaskHandle_t lvgl_tick_Task_Handler;
 TaskHandle_t lvgl_timer_Task_Handler;
 TaskHandle_t Task1_Handler;
-SemaphoreHandle_t uart2_mutex_handler;
+SemaphoreHandle_t uart2_mutex_handler,dht11_mutex_handler;
 void lvgl_tick_task(void *pvParameters)
 {
     while(1)
@@ -54,18 +57,22 @@ void lvgl_timer_task(void *pvParameters)
         vTaskDelay(pdMS_TO_TICKS(5));
     }
 }
+uint8_t temp,humi;
 void task1(void *pvParameters)
 {
     DHT11_Init(); 
-    uint8_t t,h;
     while(1)
     {
-        DHT11_Read_Data(&t,&h);
-        if(xSemaphoreTake(uart2_mutex_handler,pdMS_TO_TICKS(100))==pdPASS){
-            printf("t:%d h:%d\n",t,h);   
-            xSemaphoreGive(uart2_mutex_handler);
-        }   
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        mutex(dht11_mutex_handler,100,
+            DHT11_Read_Data(&temp,&humi);
+        )
+        
+        mutex(uart2_mutex_handler,100,
+            printf("t:%d h:%d\n",temp,humi);  
+        )
+             
+            
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
 
@@ -96,9 +103,10 @@ int main(void)
     
 
     uart2_mutex_handler= xSemaphoreCreateMutex();
-    xTaskCreate(lvgl_tick_task,"lvgl_tick_task",100,NULL,14,&lvgl_tick_Task_Handler);
+    dht11_mutex_handler= xSemaphoreCreateMutex();
+    xTaskCreate(lvgl_tick_task,"lvgl_tick_task",64,NULL,14,&lvgl_tick_Task_Handler);
     xTaskCreate(lvgl_timer_task,"lvgl_timer_task",1000,NULL,5,&lvgl_timer_Task_Handler);
-    xTaskCreate(task1,"task1",256,NULL,13,&Task1_Handler);
+    xTaskCreate(task1,"task1",128,NULL,5,&Task1_Handler);
     vTaskStartScheduler();
 
 	while(1)
